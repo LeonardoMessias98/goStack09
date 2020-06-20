049e615473f1efcb91b5dd4ea66c90fb6cbdfb5e
LeonardoMessias98 <messiasleonardo98@gmail.com>
2020-06-20 20:11:55 -0300

    subindo backend

---
+++ backend/src/app/controllers/UserController.js
@@ -0,0 +1,89 @@
+import * as Yup from "yup";
+
+import User from "../models/User";
+
+class UserController {
+  async index(req, res){
+    return res.json(await User.findAll())
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation failed" });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists." });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+    
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation failed" });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const emailExists = await User.findOne({
+        where: { email },
+      });
+
+      if (emailExists) {
+        return res.status(400).json({ error: "User already exists." });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Password does not match" });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+    console.log(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
---
+++ backend/src/config/authConfig.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "4f15bb7c14bacde14ef5fbbbaa24f177",
+  expiresIn: "7d",
+};
